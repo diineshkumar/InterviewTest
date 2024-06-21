b76964764d8ab9fe808f52a043e38dbb3a7e7841
diineshkumar <99236084+diineshkumar@users.noreply.github.com>
2024-06-21 15:44:46 +0530

    Add files via upload

---
+++ FileObserver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace HYPERHIRE_Assignment
+{
+    public static class FileObserver
+    {
+        static string targetFilePath = "E:\\DiineshKumar\\Interview2024\\newFile.txt";
+        static DateTime lastCheckedTime = DateTime.MinValue;
+
+        public static void ElapsedTime(object sender, ElapsedEventArgs e)
+        {
+            DateTime lastWriteTime = File.GetLastWriteTime(targetFilePath);
+
+            if (lastWriteTime > lastCheckedTime) // Checking if file is modified
+            {
+                Console.WriteLine($"Change detected in the file at {lastWriteTime}.");
+                lastCheckedTime = lastWriteTime;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using HYPERHIRE_Assignment;
+using System.IO;
+using System.Threading;
+using System.Timers;
+using HYPERHIRE_Assignment;
+
+Console.WriteLine("Hello, World!");
+System.Timers.Timer tmr = new System.Timers.Timer();
+tmr.Interval = 15000; // 15 seconds
+tmr.Elapsed += FileObserver.ElapsedTime;
+tmr.Start();
+
+Console.WriteLine("Monitoring changes in the target file every 15 seconds. Press any key to exit.");
+Console.ReadKey();
